e843102f88405ea2efa0edcd20631c8a24a0a074
wicked-smart <marzaanvan@gmail.com>
2023-11-25 12:56:49 +0530

    update /query endpoint to handle multiple filters and regex based query

--- ingestor/serializers.py
+++ ingestor/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import *
+from rest_framework.exceptions import ValidationError
+import re
 
 class LogEntrySerializer(serializers.ModelSerializer):
 
@@ -23,3 +25,45 @@ class LogEntrySerializer(serializers.ModelSerializer):
         data["parentResourceId"] = metadata.get("parentResourceId")
         return data
         
+
+class LogQueryParamsSerializer(serializers.ModelSerializer):
+
+    from_timestamp = serializers.CharField(required=False, max_length=100)
+    to_timestamp = serializers.CharField(required=False, max_length=100)
+
+    class Meta:
+        model = LogEntry
+        fields = ["level", "message", "resourceId", "timestamp", "traceId", "spanId", "commit", "parentResourceId", "from_timestamp", "to_timestamp"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields.keys():
+            self.fields[field_name].allow_null=True
+            self.fields[field_name].required=False
+
+    
+    def validate(self, attrs):
+
+        timestamp = attrs.get("timestamp")
+
+        from_timestamp= attrs.get("from_timestamp")
+        to_timestamp = attrs.get("to_timestamp")
+
+        if (from_timestamp and (to_timestamp is None)) or (to_timestamp and (from_timestamp is None)):
+            raise ValidationError("from_timestamp and to_timestamp is both neccessary!")
+
+        if timestamp and (from_timestamp and to_timestamp):
+            raise ValidationError("timestamp field cannot be as a filter with timestamp range (from and to)!")
+
+        #validate from and to timestamp's format
+        if from_timestamp and to_timestamp:
+            pattern = re.compile(r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z")
+            print(type(from_timestamp))
+            match1 = pattern.fullmatch(from_timestamp)
+            match2 = pattern.fullmatch(to_timestamp)
+
+            if match1 is None or match2 is None:
+                raise ValidationError("Timestamp must be int UTC format YYYY-mm-ddTHH:MM:SSZ")
+
+
+        return attrs
--- ingestor/views.py
+++ ingestor/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import *
 from .serializers import *
+from datetime import date, datetime
 
 
 # Create your views here.
@@ -28,14 +29,43 @@ def query(request):
     if request.method == 'POST':
 
         data = request.data
-        try:
-            message = data.pop("message")
-            logs = LogEntry.objects.filter(**data, message__icontains=message)
-        except LogEntry.DoesNotExists:
-            return Response({"message": "No such logs exists!"}, status=status.HTTP_400_BAD_REQUEST)
+        queryParamSerializer = LogQueryParamsSerializer(data=data)
+        if queryParamSerializer.is_valid():
+            try:
+                message = queryParamSerializer.validated_data.pop("message")
+                
+                 
+                from_timestamp = queryParamSerializer.validated_data.get("from_timestamp")
+                to_timestamp = queryParamSerializer.validated_data.get("to_timestamp")
 
-        serializer = LogEntrySerializer(logs, many=True,  context={"type": "query"})
-        return Response(serializer.data, status=status.HTTP_200_OK)
+                if from_timestamp and to_timestamp:
+                    from_timestamp_val = queryParamSerializer.validated_data.pop("from_timestamp")
+                    to_timestamp_val = queryParamSerializer.validated_data.pop("to_timestamp")
+
+
+                # get the regex'ed query first then filter remaining on top of it
+                raw_message = rf"{message}"
+                logs = LogEntry.objects.filter(message__regex=raw_message)
+                logs = logs.filter(**queryParamSerializer.validated_data)
+        
+                
+                if from_timestamp and to_timestamp:
+                    from_timestamp = datetime.strptime(from_timestamp_val, "%Y-%m-%dT%H:%M:%SZ")
+                    to_timestamp = datetime.strptime(to_timestamp_val, "%Y-%m-%dT%H:%M:%SZ")
+                    logs = logs.filter(timestamp__gte=from_timestamp, timestamp__lte=to_timestamp)
+                
+
+
+                ## ADD PAGINATION and ROLE BASED / meaning permission class appropriate
+                serializer = LogEntrySerializer(logs, many=True,  context={"type": "query"})
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
+            except LogEntry.DoesNotExists:
+                return Response({"message": "No such logs exists!"}, status=status.HTTP_200_OK)
+
+        else:
+            return Response(queryParamSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
         
 
 
